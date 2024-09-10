9eae7b58fb2a0855887a40bc52c4126b33abe948
Rangerson Clemente <rangerson14@outlook.com>
2024-09-10 18:29:26 -0300

    Create teste.sql

---
+++ teste.sql
@@ -0,0 +1,9 @@
+use controle_acesso_ifto;
+
+SELECT * FROM gerenciar_controle_ifto_pessoa;
+SELECT * FROM gerenciar_controle_ifto_rfid;
+SELECT * FROM gerenciar_controle_ifto_historico_acesso_campus;
+SELECT * FROM gerenciar_controle_ifto_papel_pessoa;
+
+SELECT P.id
+ALTER TABLE gerenciar_controle_ifto_historico_acesso_campus SET data_acesso 
