f8a95c38999efe348ec5a854507c8a5e28f43751
Rangerson Clemente <rangerson14@outlook.com>
2024-11-26 14:02:44 -0300

    Apagando comentários dos arquivos HTML

--- gerenciar_controle_ifto/view/rfid.py
+++ gerenciar_controle_ifto/view/rfid.py
@@ -142,7 +142,7 @@ def listarRFID(request):
 
             # Aqui fazer a filtragem pelo RFID, Função, Ativo/Não Ativo ou Disponível/Não disponível
 
-            
+
             rfids = Rfid.objects.filter(tag_rfid_value__icontains=campo.upper())
 
             if len(rfids) <=0:
